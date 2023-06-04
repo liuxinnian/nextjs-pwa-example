384791d23e616865b5ed0486a29bb2f2650553f4
Xinnian Liu <xinnian.liu.us@gmail.com>
2023-06-04 18:01:11 +0800

    Initialized PWA support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = withPWA(nextConfig)
